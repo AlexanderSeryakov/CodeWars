3ebffce2b4c85bf8d9b306b45739a9e3f7b59e87
Alexander Seryakov <seryakov78116@gmail.com>
2023-01-02 19:02:29 +0300

    Added task description and link to the task

--- List_Filtering/strings_filtered_out.py
+++ List_Filtering/strings_filtered_out.py
@@ -1,5 +1,11 @@
+"""In this kata you will create a function that takes a list of non-negative integers and strings and returns a new
+list with the strings filtered out."""
+
+
 def filter_list(l):
     return [x for x in l if type(x) is int]
+
+# Link: https://www.codewars.com/kata/53dbd5315a3c69eed20002dd/train/python
 
 
 assert filter_list([1, 2, 'a', 'b']) == [1, 2], 'Incorrect filtered out'
